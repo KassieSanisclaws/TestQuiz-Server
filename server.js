d3a0dc82427864e5f3e7b997af5cf3cb34bc6260
KassieSanisclaws <86675041+KassieSanisclaws@users.noreply.github.com>
2022-10-15 05:06:55 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+//DotEnv- //
+dotenv.config();
+
+//Create express app.//
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Header", "Content-Type, Acceptm X-Requested-With, Origin");                                                                                        
+    next();
+});
+
+//Api-Routes.//
+//working on api route.
+// const test1Questions = require("");
+
+//Error-Message Handler On Server.//                                                                                                                                                                                                   
+app.use((err, req, res, next) => {                                                                                                                                                                                                                                                                        
+    res.status(500).send({ message: err.message });                           
+});              
+
+//Server Route Connection-(respond on successful connection).//
+app.get("/", (req, res) => {
+    res.send("Server Is Ready");
+});
+
+//Server Port Listening-On/ Serving-On.//
+const port = process.env.PORT || 54441;
+app.listen(port, () => {
+    console.log(`Server Is Ready At http://localhost:${port}`);
+});
+
+module.exports = app;
